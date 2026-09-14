4a05d0a18bef49e644613577b001c7ddd9c62613
vinodkumar <vinodkumar.dscigmail.com>
2026-09-14 19:41:10 +0530

    added the headless options

--- src/main/java/com/framework/base/DriverFactory.java
+++ src/main/java/com/framework/base/DriverFactory.java
@@ -1,10 +1,10 @@
 package com.framework.base;
 
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -14,29 +14,40 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-    
 
     public static WebDriver initDriver(String browser) {
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            driver.set(new ChromeDriver());
+            
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless=new");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeOptions.addArguments("--window-size=1920,1080");
+            
+            driver.set(new ChromeDriver(chromeOptions));
+            
         } else if (browser.equalsIgnoreCase("firefox")) {
-        	System.setProperty(ConfigReader.get("webdriverGecko"), ConfigReader.get("webdriverLocalPath"));
+            System.setProperty(ConfigReader.get("webdriverGecko"), ConfigReader.get("webdriverLocalPath"));
             
-        	FirefoxOptions options = new FirefoxOptions();
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
             options.setAcceptInsecureCerts(true);
-            // Specify the exact binary path to your firefox.exe
-            options.setBinary(ConfigReader.get("firefoxBinarypath")); 
-           
-            driver.set(new FirefoxDriver(options));
-        } else if(browser.equalsIgnoreCase("edge"))
-        {
-        	EdgeOptions edgeOptions = new EdgeOptions();
-            edgeOptions.addArguments("--remote-allow-origins=*");
-        	System.setProperty(ConfigReader.get("webdriverEdge"), ConfigReader.get("webdriverLocpath"));
+            options.setBinary(ConfigReader.get("firefoxBinarypath"));
             
-            driver.set(new EdgeDriver());
+            driver.set(new FirefoxDriver(options));
+            
+        } else if (browser.equalsIgnoreCase("edge")) {
+            System.setProperty(ConfigReader.get("webdriverEdge"), ConfigReader.get("webdriverLocpath"));
+            
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.addArguments("--headless=new");
+            edgeOptions.addArguments("--disable-gpu");
+            edgeOptions.addArguments("--window-size=1920,1080");
+            edgeOptions.addArguments("--remote-allow-origins=*");
+            
+            driver.set(new EdgeDriver(edgeOptions));
         }
+        
         driver.get().manage().window().maximize();
         return driver.get();
     }
